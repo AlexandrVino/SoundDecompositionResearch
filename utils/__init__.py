30327d8f92db8b3b09d02b33dcefee8d307f0cb5
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2022-09-21 14:17:59 +0500

    add description for utils module

---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+"""
+
+Python module utils
+
+Files:
+
+load.py -consist all functions to reading data from *.txt, *.json, *.wav, *.mp3 file formats
+write.py - consist all functions to write data to *.txt, *.json file formats
+
+"""
